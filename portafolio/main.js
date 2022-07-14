691f3386030a15b88e41c3535d18d7d4636873c5
samaba-God-777 <degraciawilliams10@gmail.com>
2022-07-13 19:31:50 -0500

    Add files via upload

---
+++ portafolio/main.js
@@ -0,0 +1,20 @@
+
+/*function sayHello(text, text2){
+    let word = ''
+    for(let i = 0; i < text.length; i++){
+      if(word[i] == text[i]){
+
+      }
+      else {
+        word[i] += text2[i] + 1
+      }
+    }
+ return word[i,j]
+}
+console.log('hello')*/
+
+
+let productos = [{nombre: 'smartphone', precio:950.00, imagen:'../z', catergoria: 'telefono mobile',cantidad:20},
+                {nombre: 'computadora', precio:450.00, imagen:'../',catergoria: 'laptop',cantidad:30},
+                {nombre: 'Ipad pro',precio:980, imagen:'../', catergoria: 'tablet',cantidad:10},
+                {nombre: 'audifonos', precio:50.00, imagen:'../',catergoria:'asesorios',cantidad:15}]
